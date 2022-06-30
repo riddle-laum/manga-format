b750fb9e2d26ad4618538418e2b42da52f9c4489
riddle <67580233+riddle-laum@users.noreply.github.com>
2022-06-30 18:27:33 +0900

    Delete build-mjs directory

--- build-mjs/type.mjs
+++
@@ -1,9 +0,0 @@
-import * as v2checks from './check.v2.mjs';
-export var v2;
-(function (v2) {
-    v2.isManga = v2checks.isManga;
-    v2.isAuthor = v2checks.isAuthor;
-    v2.isSeries = v2checks.isSeries;
-    v2.isTag = v2checks.isTag;
-})(v2 || (v2 = {}));
-;
